123890fdabe77a124da07e77b492dd65978cbc9d
Matti Ullah Thaheem <139385194+Matti251@users.noreply.github.com>
2024-01-04 10:25:23 +0500

    E commere

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Navigation/Nav";
+import Products from "./View/products/Products";
+import Recommended from "./components/Recommended/Recommended";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { data as productss } from "./utils/Data";
+import Card from "./components/Card";
+import Colors from "./components/Colors/Colors";
+import "./style/App.css";
+import { useFilterState } from "./hooks/filter-hook";
+import SideBar from "./components/Sidebar/SideBar";
+const App = () => {
+  const { filter, setFilter } = useFilterState();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <SideBar />
+      <Colors
+        Setcolor={(val) =>
+          setFilter({ ...filter, color: val })
+        }
+      />
+      <Recommended
+        setCompany={(val) =>
+          setFilter({ ...filter, company: val })
+        }
+      />
+      <Products>
+        {productss
+          .filter(
+            (item) =>
+              item.category.includes(
+                filter.category
+              ) &&
+              item.newPrice.includes(
+                filter.price
+              ) &&
+              item.company.includes(
+                filter.company
+              ) &&
+              item.title
+                .toLowerCase()
+                .includes(
+                  filter.title.toLowerCase()
+                ) &&
+              item.color.includes(filter.color)
+          )
+          .map((item, i) => (
+            <Card
+              key={"product-car--" + i}
+              data={item}
+            />
+          ))}
+      </Products>
+    </>
   );
-}
+};
 
 export default App;
